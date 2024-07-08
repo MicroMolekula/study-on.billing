e2d530b70ef7b32e241b620e5808ac8a5eb5782c
luke <ivan1.krasikov.2003@gmail.com>
2024-07-08 15:34:51 +0300

    downloaded packages and connected, created db

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $hasher,
+    ) {  
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        
+
+        $manager->flush();
+    }
+}
